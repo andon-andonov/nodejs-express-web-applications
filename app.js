df323caa0ddae530d903ea703d3b84f3e5a0c19a
Andon Andonov <andon.andonov@gmail.com>
2019-10-06 16:14:11 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
+
+const app = express();
+
+app.use(morgan('tiny'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.listen(4000, () => {
+    debug(`listening on port ${chalk.green(4000)}`);
+});
