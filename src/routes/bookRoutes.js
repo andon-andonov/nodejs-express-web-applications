88e65db1df74454f5ed24787e495743130a1b6e6
Andon Andonov <andon.andonov@gmail.com>
2019-10-12 00:11:44 +0300

    update detail view to get book from mongo db

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectID } = require('mongodb');
 const debug = require('debug')('app:bookRoutes');
 
 const bookRouter = express.Router();
@@ -37,11 +37,30 @@ function router(nav) {
   bookRouter.route('/:id')
     .get((req, res) => {
       const { id } = req.params;
-      res.render('bookDetailView', {
-        nav,
-        title: 'Library',
-        book: null,
-      });
+      const url = 'mongodb://localhost:27017';
+      const dbName = 'libraryApp';
+      (async function mongo() {
+        let client;
+        try {
+          client = await MongoClient.connect(url,
+            { useNewUrlParser: true, useUnifiedTopology: true });
+          debug('Connected correctly to server');
+          const db = client.db(dbName);
+          const col = await db.collection('books');
+          const book = await col.findOne({ _id: new ObjectID(id) });
+          res.render('bookDetailView', {
+            nav,
+            title: 'Library',
+            book,
+          });
+        } catch (err) {
+          debug(err.stack);
+        } finally {
+          if (client) {
+            client.close();
+          }
+        }
+      }());
     });
 
   return bookRouter;
