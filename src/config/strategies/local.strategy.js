b35ff93f7cd598714a405968bd3ba368f90564bd
Andon Andonov <andon.andonov@gmail.com>
2019-10-13 01:00:14 +0300

    add signin form and storing users in mongo

--- src/config/strategies/local.strategy.js
+++ src/config/strategies/local.strategy.js
@@ -1,14 +1,37 @@
 const passport = require('passport');
 const { Strategy } = require('passport-local');
+const { MongoClient } = require('mongodb');
+const debug = require('debug')('app:local.strategy');
 
 module.exports = function localStrategy() {
   passport.use(new Strategy({
     usernameField: 'username',
     passwordField: 'password',
   }, (username, password, done) => {
-    const user = {
-      username, password,
-    };
-    done(null, user);
+    const url = 'mongodb://localhost:27017';
+    const dbName = 'libraryApp';
+
+    (async function addUser() {
+      let client;
+      try {
+        client = await MongoClient.connect(url,
+          { useNewUrlParser: true, useUnifiedTopology: true });
+        debug('Connected correctly to server');
+        const db = client.db(dbName);
+        const col = await db.collection('users');
+        const user = await col.findOne({ username });
+        if (user && user.password === password) {
+          done(null, user);
+        } else {
+          done(null, false);
+        }
+      } catch (err) {
+        debug(err.stack);
+      } finally {
+        if (client) {
+          client.close();
+        }
+      }
+    }());
   }));
 };
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,17 +1,50 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
 const debug = require('debug')('app:authRoutes');
+const passport = require('passport');
 
 const authRouter = express.Router();
 
 function router() {
   authRouter.route('/signup')
     .post((req, res) => {
-      debug(req.body);
-      req.login(req.body, () => {
-        res.redirect('/auth/profile');
-      });
+      const { username, password } = req.body;
+      const url = 'mongodb://localhost:27017';
+      const dbName = 'libraryApp';
+
+      (async function addUser() {
+        let client;
+        try {
+          client = await MongoClient.connect(url,
+            { useNewUrlParser: true, useUnifiedTopology: true });
+          debug('Connected correctly to server');
+          const db = client.db(dbName);
+          const col = await db.collection('users');
+          const user = { username, password };
+          const results = await col.insertOne(user);
+          req.login(results.ops[0], () => {
+            res.redirect('/auth/profile');
+          });
+        } catch (err) {
+          debug(err.stack);
+        } finally {
+          if (client) {
+            client.close();
+          }
+        }
+      }());
     });
+
+  authRouter.route('/signin')
+    .get((req, res) => {
+      res.render('signin', {
+        title: 'Sign In',
+      });
+    })
+    .post(passport.authenticate('local', {
+      successRedirect: '/auth/profile',
+      failureRedirect: '/',
+    }));
 
   authRouter.route('/profile')
     .get((req, res) => {
